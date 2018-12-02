0d00edef259e30fad3884da718e98846dab303e4
DESKTOP-96KRNN2\motta <benmotta@gmail.com>
2018-12-02 07:34:35 -0700

    Modulos de psicologos

---
+++ application/modules/psicologo/controllers/Psicologo.php
@@ -0,0 +1,40 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Psicologo extends MX_Controller {
+	
+    public function __construct() {
+        parent::__construct();
+    }
+
+	/**
+	 * Index Page for this controller.
+	 */
+	public function index()
+	{	
+			$this->session->sess_destroy();
+			$this->load->view('home');
+	}
+	
+	/**
+	 * Zona psicologos
+	 */
+	public function zona_psicologos()
+	{	
+			$data["view_header"] = 'template/header_psicologos';
+			$data["view"] = 'zona_psicologos';
+			$this->load->view("layout_secundario", $data);
+	}
+	
+	/**
+	 * Contacto
+	 */
+	public function contacto()
+	{	
+			$data["view_header"] = 'template/header_contacto';
+			$data["view"] = 'contacto';
+			$this->load->view("layout_secundario", $data);
+	}
+
+	
+}
---
+++ application/modules/psicologo/views/form_psicologo.php
@@ -0,0 +1,166 @@
+  <section class="section section-lg bg-white novi-background bg-image">
+    <div class="container">
+      <div class="row">
+        <div class="col-sm-10 col-lg-6">
+          <div class="feature-box-3">
+            <h5>Welcome to Gova</h5>
+            <h3>Your Complete Web Solution Partner</h3>
+            <p>Let's make the lorem ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's sit amet auctor lacus vestibulum standard dummy text ever since.</p>
+            <p>Let's make the lorem ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's sit amet auctor standard dummy text ever since.</p>
+            <a class="btn btn-primary btn-lg btn-effect-ujarak" href="#"><span>Registro nuevos</span></a>
+			<a class="btn btn-primary btn-lg btn-effect-ujarak" href="<?php echo base_url("login"); ?>"><span>Inicio sesión</span></a>
+		  </div>
+        </div>
+        <div class="col-sm-10 col-lg-6"><img src="images/img-01.png" alt="" class="img-responsive"></div>
+      </div>
+      <div class="row mar-top-8">
+        <div class="col-sm-6 col-lg-4">
+          <article class="box-minimal unit unit-xs-horizontal unit-spacing-sm">
+            <div class="unit-left">
+              <div class="box-minimal-icon">
+                <div class="novi-icon fl-bigmug-line-headphones32 text-purple"></div>
+              </div>
+            </div>
+            <div class="unit-body pad-left-35">
+              <h5 class="box-minimal-title"><a href="#">Highly Customizable</a></h5>
+              <div class="box-minimal-text">Let's make lorem ipsum is simply text the printing and typesetting standard industry.</div>
+            </div>
+          </article>
+        </div>
+        <div class="col-sm-6 col-lg-4">
+          <article class="box-minimal unit unit-xs-horizontal unit-spacing-sm">
+            <div class="unit-left">
+              <div class="box-minimal-icon">
+                <div class="novi-icon fl-bigmug-line-two317 text-purple"></div>
+              </div>
+            </div>
+            <div class="unit-body pad-left-35">
+              <h5 class="box-minimal-title"><a href="#">Highly Customizable</a></h5>
+              <div class="box-minimal-text">Let's make lorem ipsum is simply text the printing and typesetting standard industry.</div>
+            </div>
+          </article>
+        </div>
+        <div class="col-sm-6 col-lg-4">
+          <article class="box-minimal unit unit-xs-horizontal unit-spacing-sm">
+            <div class="unit-left">
+              <div class="box-minimal-icon">
+                <div class="novi-icon fl-bigmug-line-cube29 text-purple"></div>
+              </div>
+            </div>
+            <div class="unit-body pad-left-35">
+              <h5 class="box-minimal-title"><a href="#">Highly Customizable</a></h5>
+              <div class="box-minimal-text">Let's make lorem ipsum is simply text the printing and typesetting standard industry.</div>
+            </div>
+          </article>
+        </div>
+      </div>
+    </div>
+  </section>
+  <section class="section section-lg bg-white novi-background bg-image">
+    <div class="container">
+      <div class="row">
+        <div class="col-sm-10 col-lg-6"><img src="images/site-img26-3.jpg" alt="" class="img-responsive bor-radius-5"></div>
+        <div class="col-sm-10 col-lg-6 text-left">
+          <h3>Web Development</h3>
+          <p>Let's make lorem ipsum is simply text the printing and typesetting ornare praesent mattis commodo augue Aliquam hendrerit augue Cras standard industry your complete solution partner.</p>
+          <ul class="marked-list marked-list__marker-1">
+            <li class="text-gray">Create your online presence</li>
+            <li class="text-gray">WAP Enable your online presence</li>
+            <li class="text-gray">Document Management Systems</li>
+            <li class="text-gray">On-going Support &amp; Enhancements</li>
+          </ul>
+          <a class="btn btn-primary btn-lg btn-effect-ujarak" href="#"><span>To know More</span></a></div>
+      </div>
+      <div class="row">
+        <div class="col-sm-10 col-lg-6 text-right">
+          <h3>Custom Website Design </h3>
+          <p>Let's make lorem ipsum is simply text the printing and typesetting ornare praesent mattis commodo augue Aliquam hendrerit augue Cras standard industry your complete solution partner.</p>
+          <ul class="marked-list sty-right marked-list__marker-1">
+            <li class="text-gray">Mapping Business Processes</li>
+            <li class="text-gray">Third Party Tools Integrations</li>
+            <li class="text-gray">Document Management Systems</li>
+            <li class="text-gray">On-going Support &amp; Enhancements</li>
+          </ul>
+          <a class="btn btn-primary btn-lg btn-effect-ujarak" href="#"><span>To know More</span></a></div>
+        <div class="col-sm-10 col-lg-6"><img src="images/site-img25-1-3.jpg" alt="" class="img-responsive bor-radius-5" style="position:relative"></div>
+      </div>
+    </div>
+  </section>
+  <section class="section section-lg bg-default novi-background bg-image">
+    <div class="container">
+      <div class="row justify-content-center spacing-30">
+        <div class="col-sm-10 col-lg-8 col-xl-6"><a href="#" class="banner-box banner-box-variant-1">
+          <figure class="wow fadeInUpSmall"><img src="images/site-img10-7.jpg" alt="" class="img-responsive"></figure>
+          <div class="caption wow fxRotateInLeft">
+            <h6>Choose Our</h6>
+            <h2>Best Services</h2>
+            <p>Let's make the amet consect adipiscing elit.</p>
+          </div>
+          </a></div>
+        <div class="col-sm-10 col-lg-8 col-xl-6"><a href="#" class="banner-box banner-box-variant-1">
+          <figure class="wow fadeInUpSmall"><img src="images/site-img9-3.jpg" alt="" class="img-responsive"></figure>
+          <div class="caption wow fxRotateInLeft">
+            <h6>Digital Branding</h6>
+            <h2>Development</h2>
+            <p>Let's make the amet consect adipiscing elit.</p>
+          </div>
+          </a></div>
+      </div>
+    </div>
+  </section>
+  <section class="section text-center vide_bg bg-gray-dark novi-background bg-image cta-wrap" data-vide-bg="mp4: video/time-lapse-video-of-night-sky-1-8.mp4, poster: images/slide-26-5.jpg" data-vide-options="posterType: jpg" style="background-image: url(&quot;images/slide-26-4.jpg&quot;);">
+    <div class="section-xl">
+      <div class="container">
+        <div class="row justify-content-center">
+          <div class="col-sm-10 col-lg-9">
+            <hr class="divider-sm divider-success">
+            <h1>We Create Stunning Websites</h1>
+            <p class="subtitle-variant-3 text-success">and Retina Ready</p>
+            <a class="btn btn-white-outline btn-lg btn-aqil btn-aqil--mod-1" href="#"><span>LET'S GET STARTED</span></a></div>
+        </div>
+      </div>
+    </div>
+  </section>
+  <section class="section section-lg bg-default novi-background bg-image">
+    <div class="container">
+      <div class="row justify-content-center spacing-40">
+        <div class="col-sm-10 col-lg-10 section-auto mar-bottom-2">
+          <h3><span class="heading-3">Our</span> Best Team</h3>
+          <p>Let's make lorem ipsum is simply text the printing and standard industry.</p>
+        </div>
+        <div class="col-sm-6 col-md-4 team-members">
+          <div class="team-member">
+            <div class="team-image"><img src="images/site-img31.jpg" alt="" class="img-responsive"></div>
+            <div class="team-desc">
+              <h3>Alex Greenfield</h3>
+              <span>Software Developer</span>
+              <p>Let's make lorem ipsum dolor sit consetetur elt dipscing elitr sed diam mod mpor invidunt tar abore.</p>
+              <div class="social-media align-center"><a href="#"><i class="fa fa-facebook"></i></a><a href="#"><i class="fa fa-twitter"></i></a><a href="#"><i class="fa fa-instagram"></i></a><a href="#"><i class="fa fa-envelope"></i></a></div>
+            </div>
+          </div>
+        </div>
+        <div class="col-sm-6 col-md-4 team-members">
+          <div class="team-member">
+            <div class="team-image"><img src="images/site-img32.jpg" alt="" class="img-responsive"></div>
+            <div class="team-desc">
+              <h3>Miriam Richmond</h3>
+              <span>Web Designer</span>
+              <p>Let's make lorem ipsum dolor sit consetetur elt dipscing elitr sed diam mod mpor invidunt tar abore.</p>
+              <div class="social-media align-center"><a href="#"><i class="fa fa-facebook"></i></a><a href="#"><i class="fa fa-twitter"></i></a><a href="#"><i class="fa fa-instagram"></i></a><a href="#"><i class="fa fa-envelope"></i></a></div>
+            </div>
+          </div>
+        </div>
+        <div class="col-sm-6 col-md-4 team-members">
+          <div class="team-member">
+            <div class="team-image"><img src="images/site-img14.jpg" alt="" class="img-responsive"></div>
+            <div class="team-desc">
+              <h3>John Richmond</h3>
+              <span>Wed Developer</span>
+              <p>Let's make lorem ipsum dolor sit consetetur elt dipscing elitr sed diam mod mpor invidunt tar abore.</p>
+              <div class="social-media align-center"><a href="#"><i class="fa fa-facebook"></i></a><a href="#"><i class="fa fa-twitter"></i></a><a href="#"><i class="fa fa-instagram"></i></a><a href="#"><i class="fa fa-envelope"></i></a></div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
