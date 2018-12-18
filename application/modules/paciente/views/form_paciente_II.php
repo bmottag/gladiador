5fe80680458bac81317f8db47db6ce96ea691641
DESKTOP-96KRNN2\motta <benmotta@gmail.com>
2018-12-17 19:21:15 -0700

    ajuste de forma

--- application/modules/paciente/views/form_paciente_II.php
+++ application/modules/paciente/views/form_paciente_II.php
@@ -5,7 +5,7 @@
 		<div class="col-md-12 col-sm-12 col-xs-12">
 			<div class="x_panel">
 				<div class="x_title">
-					<h2><i class='fa fa-users'></i> Cuestionario Paciente</h2>
+					<h2><i class='fa fa-users'></i> Encontrar mi psicólogo</h2>
 					<ul class="nav navbar-right panel_toolbox">
 						<li><a class="collapse-link"><i class="fa fa-chevron-up"></i></a>
 						</li>
@@ -15,7 +15,7 @@
 				<div class="x_content">
 
 					<div class="alert alert-success alert-dismissible fade in" role="alert">
-						<strong>Info:</strong> Cuestionario para perfilar al paciente de acuerdo a las respuestas que da. <small>(II)</small>
+						<strong>Completa el cuestionario TuApoyo y encuentra el mejor psicólogo para ti</strong> <small>(II)</small>
 					</div>
 
 					<div class="progress progress-striped progress_wide">
